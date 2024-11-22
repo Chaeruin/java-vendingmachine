f52f15d861093d094142ed4bf57e9d32974a0b1f
Chaeruin <cms3604@naver.com>
2024-11-22 14:51:10 +0900

    feat: 테케 통과, 구현 완료, 잔돈 랜덤 배치 구현 완료

--- src/main/java/vendingmachine/controller/ChangeController.java
+++ src/main/java/vendingmachine/controller/ChangeController.java
@@ -2,6 +2,7 @@ package vendingmachine.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import vendingmachine.domain.Money;
 import vendingmachine.enums.Coin;
 
@@ -13,20 +14,21 @@ public class ChangeController {
     public Map<Coin, Integer> getChange(Map<Coin, Integer> coinAmount, Money input) {
         int remain = input.getMoney();
         Map<Coin, Integer> change = initMap();
-        while (remain > 0) {
-            // while/for 종료 고려
-            setChange(remain, coinAmount, change);
-        }
+        // while/for 종료 고려!! 역시나 여기가 문제
+        setChange(remain, coinAmount, change);
         return change;
     }
 
     public void setChange(int remain, Map<Coin, Integer> coinAmount, Map<Coin, Integer> change) {
-        for (Coin c : Coin.values()) {
-            if (coinAmount.get(c) != 0 && (remain - c.getAmount() >= 0)) {
-                remain -= c.getAmount();
-                change.replace(c, change.get(c) + 1);
-                coinAmount.replace(c, coinAmount.get(c) - 1);
+        for (Entry<Coin, Integer> ca : coinAmount.entrySet()) {
+            if (remain - ca.getKey().getAmount() >= 0 && ca.getValue() != 0) {
+                while (remain > 0 && coinAmount.get(ca.getKey()) != 0) {
+                    remain -= ca.getKey().getAmount();
+                    change.replace(ca.getKey(), change.get(ca.getKey()) + 1);
+                    coinAmount.replace(ca.getKey(), coinAmount.get(ca.getKey()) - 1);
+                }
             }
+
         }
     }
 
