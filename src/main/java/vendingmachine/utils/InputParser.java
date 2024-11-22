30c46a2717f614f7af10dec3459830ef5a958ba9
Chaeruin <cms3604@naver.com>
2024-11-22 12:11:36 +0900

    feat: utils 설계 및 정규식 서치

--- src/main/java/vendingmachine/utils/InputParser.java
+++ src/main/java/vendingmachine/utils/InputParser.java
@@ -1,4 +1,29 @@
 package vendingmachine.utils;
 
+import vendingmachine.domain.Inventory;
+import vendingmachine.domain.Money;
+import vendingmachine.domain.Product;
+
 public class InputParser {
+    public static Money parseMoney(String input) {
+        if (InputValidator.isParsingErrorOrNone(input)) {
+            return new Money(Integer.parseInt(input));
+        }
+        return null;
+    }
+
+    public static Inventory parseInventory(String input) {
+        String regex = "[\\[\\]]";
+        String[] inputSplit = input.replaceAll(regex, "")
+                .replaceAll(";", ",").split(",");
+        for (int i = 0; i < inputSplit.length; i += 3) {
+            if (InputValidator.isParsingErrorOrNone(inputSplit[i + 1]) && InputValidator.isParsingErrorOrNone(
+                    inputSplit[i + 2])) {
+                return new Inventory(new Product(inputSplit[i], Integer.parseInt(inputSplit[i + 1])),
+                        Integer.parseInt(inputSplit[i + 2]));
+            }
+        }
+        return null;
+    }
+
 }
--- src/main/java/vendingmachine/utils/InputValidator.java
+++ src/main/java/vendingmachine/utils/InputValidator.java
@@ -1,4 +1,26 @@
 package vendingmachine.utils;
 
+import vendingmachine.enums.ErrorMessage;
+
 public class InputValidator {
+
+    // 자판기 금액 문/빈 예외
+    // 가격, 수량 파싱할 때 해당 부분에 넣으면 됨
+    // 투입 금액 문/빈 예외
+    public static boolean isParsingErrorOrNone(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
+        }
+        return true;
+    }
+
+    // 구매할 때 사용
+    public static boolean isNoneProduct(String input) {
+        if (input.equals("") || input.equals(" ")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
+        }
+        return true;
+    }
 }
